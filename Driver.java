ea5a97712bf15a0bc5cf95c0d77bee37994ae78f
marcello <marcellopalucci@gmail.com>
2024-04-09 20:52:35 -0400

    basically final

--- Driver.java
+++ Driver.java
@@ -4,44 +4,51 @@ public class Driver {
     public static void main(String[] args) {
 
         Member member1 = new Member("Otto", Group.BACKEND, 60);
-        Member member2 = new Member("John", Group.ADMIN, 40);
+        Member member2 = new Member("John", Group.ADMIN, 50);
         Member member3 = new Member("Ash", Group.FRONTEND, 15);
         Member member4 = new Member("Ryan", Group.FRONTEND, 37);
         Member member5 = new Member("John", Group.ADMIN, 40);
         Member member6 = new Member("Edy", Group.FRONTEND, 45);
-
         Member fakeMember = new Member("Lionel", Group.ADMIN, 30);
 
         Member[] teamArr = new Member[]{member1, member2, member3, member4, member5, member6};
         Member[] teamArr1 = new Member[]{member1, member2, member3};
         Member[] teamArr2 = new Member[]{member6, member3, member5};
-        Member[][] twoDTeam = new Member[][] {{member1, member2, member3}, {member1, member2, member3}};
+        Member[][] twoDTeam = new Member[][] {teamArr1, teamArr2};
+
         Team team1 = new Team(teamArr);
 
         System.out.println("Unsorted:");
-        for (int i = 0; i < teamArr.length; i++) {
-            System.out.println(team1.getMembers()[i]);
-        }
+        printArray(team1);
 
         team1.mergeSortMembers();
         System.out.println("\nSorted:");
-        for (int i = 0; i < teamArr.length; i++) {
-            System.out.println(team1.getMembers()[i]);
-        }
+        printArray(team1);
+
         System.out.println("\nFound:");
         System.out.println(team1.searchMember(member1));
+        //System.out.println(team1.searchMember(fakeMember));
+
 
         Team reversedTeam = new Team(team1.reverseMembers());
         System.out.println("\nReversed:");
-        for (int i = 0; i < teamArr.length; i++) {
-            System.out.println(reversedTeam.getMembers()[i]);
-        }
+        printArray(reversedTeam);
 
         ArrayList<Member> leaders = team1.selectLeaderBoard();
-        System.out.println("Leaders:");
+        System.out.println("\nLeaders:");
         System.out.println(leaders.get(0));
         System.out.println(leaders.get(1));
 
         team1.mergeAllMembers(twoDTeam);
+        System.out.println("\nMerge all members:");
+        printArray(team1);
     }
+
+    private static void printArray(Team t) {
+        int size = t.getMembers().length;
+        for (Member i : t.getMembers()){
+            System.out.println(i);
+        }
+    }
+
 }
--- Team.java
+++ Team.java
@@ -1,21 +1,23 @@
 import java.util.NoSuchElementException;
 import java.util.ArrayList;
-import java.lang.Math;
 import java.util.Random;
 
+/**
+ * Team is a class that holds the recursive methods to easily manage a team.
+ * @author mpalucci3
+ * @version 1.2
+ */
 public class Team {
     private Member[] members;
     /**
-     * Constructor which takes in and array of Members.
+     * Constructor which takes in and array of Members and makes a deep copy of the passed in array.
      * @param members array of Member instances that store all the members of
      *                your team.
      */
-    public Team(Member[] members){
+    public Team(Member[] members) {
         int memberSize = members.length;
-        //shallow copy
-        //this.members = members;
         this.members = new Member[memberSize];
-        for(int i = 0; i < memberSize; i++){
+        for (int i = 0; i < memberSize; i++) {
             this.members[i] = members[i];
         }
 
@@ -23,13 +25,14 @@ public class Team {
 
     /**
      * The mergeSortMembers method uses the merge sort algorithm
-     * to sort the Members array.
+     * to sort the Members array in non-decreasing order. This method does not take any arguments nor
+     * return anything.
      */
     public void mergeSortMembers() {
         members = recursiveMergeMembers(members);
     }
-    private Member[] recursiveMergeMembers(Member[] original){
-        if(original.length > 1){
+    private Member[] recursiveMergeMembers(Member[] original) {
+        if (original.length > 1) {
             int length = original.length;
             int middle = length / 2;
 
@@ -44,22 +47,32 @@ public class Team {
         return original;
     }
 
+    /**
+     * This method takes in a 2-D array of Member instances and does not return anything.
+     * @param twoDMembers 2-D array representing Member instances to be merged and sorted
+     */
     public void mergeAllMembers(Member[][] twoDMembers) {
         mergeSortMembers();
         int i = 0;
         members = recursiveMergeAllMembers(twoDMembers, members, i);
         mergeSortMembers();
     }
-    public Member[] recursiveMergeAllMembers(Member[][] twoDMembers, Member[] original, int i){
-        if (i < twoDMembers.length - 1){
+    private Member[] recursiveMergeAllMembers(Member[][] twoDMembers, Member[] original, int i) {
+        if (i < twoDMembers.length - 1) {
             Member[] mergedMembers = HWUtils.merge(twoDMembers[i], twoDMembers[i + 1]);
             recursiveMergeAllMembers(twoDMembers, mergedMembers, ++i);
         }
         return HWUtils.merge(members, original);
     }
 
+    /**
+     * This method takes in a Member instance and returns the Member instance in the members
+     * array that has the same name, subgroup, and hoursWorked.
+     * @param m Member representing a member in the team to be searched for in the
+     * @return Member representing the found member
+     */
     public Member searchMember(Member m) {
-        if (m == null){
+        if (m == null) {
             throw new IllegalArgumentException("Member cannot be null");
         }
         mergeSortMembers();
@@ -67,19 +80,19 @@ public class Team {
         int high = members.length - 1;
 
         int foundIndex = recursiveSearchMember(low, high, m);
-        if (foundIndex == -1){
+        if (foundIndex == -1) {
             throw new NoSuchElementException("Member not found in the member array");
-        }else {
-            return members[foundIndex] ;
+        } else {
+            return members[foundIndex];
         }
     }
 
     private int recursiveSearchMember(int low, int high, Member key) {
         int mid = (high + low) / 2;
-        if (low > high){
+        if (low > high) {
             return -1;
         }
-        if (members[mid].equals(key)){
+        if (members[mid].equals(key)) {
             return mid;
         } else if (members[mid].compareTo(key) < 0) {
             return recursiveSearchMember(mid + 1, high, key);
@@ -87,30 +100,41 @@ public class Team {
             return recursiveSearchMember(low, mid - 1, key);
         }
     }
+
+    /**
+     * This method does not take in any arguments and returns a deep copy of the members array in reversed sorted order.
+     * @return Member[] representing the array in reverse order
+     */
     public Member[] reverseMembers() {
         Member[] reversedMembers = new Member[members.length];
         int i = members.length - 1;
         return recursiveReverseMembers(reversedMembers, i);
     }
 
-    private Member[] recursiveReverseMembers(Member[] reversedMembers, int i){
-        if (i >= 0){
+    private Member[] recursiveReverseMembers(Member[] reversedMembers, int i) {
+        if (i >= 0) {
             reversedMembers[members.length - i - 1] = members[i];
             recursiveReverseMembers(reversedMembers, --i);
         }
         return reversedMembers;
     }
 
-    public ArrayList<Member> selectLeaderBoard(){
+    /**
+     * This method takes in no arguments, and returns a randomly selected leader for the
+     * BACKEND and FRONTEND group.
+     * @return ArrayList of type Member, where index 0 represents the leader for the FRONTEND group
+     *         and index 1 represents the learder for the BACKEND group
+     */
+    public ArrayList<Member> selectLeaderBoard() {
         ArrayList<Member> backEndGroup = new ArrayList<>();
         ArrayList<Member> frontEndGroup = new ArrayList<>();
-        int i =0;
+        int i = 0;
 
         return recursiveLeaderBoard(backEndGroup, frontEndGroup, i);
     }
 
-    private ArrayList<Member> recursiveLeaderBoard(ArrayList<Member> backEnd, ArrayList<Member> frontEnd, int i){
-        if (i < members.length){
+    private ArrayList<Member> recursiveLeaderBoard(ArrayList<Member> backEnd, ArrayList<Member> frontEnd, int i) {
+        if (i < members.length) {
             if (members[i].getSubgroup() == Group.BACKEND) {
                 backEnd.add(members[i]);
                 return recursiveLeaderBoard(backEnd, frontEnd, ++i);
@@ -122,7 +146,7 @@ public class Team {
             }
         }
 
-        if (backEnd.isEmpty() && frontEnd.isEmpty()){
+        if (backEnd.isEmpty() && frontEnd.isEmpty()) {
             throw new NoSuchElementException("Failed to select leaders for both subgroups.");
         } else if (frontEnd.isEmpty()) {
             throw new NoSuchElementException("Failed to select a leader for the FRONTEND subgroup.");
@@ -130,14 +154,29 @@ public class Team {
             throw new NoSuchElementException("Failed to select a leader for the BACKEND subgroup.");
         }
         ArrayList<Member> leaders = new ArrayList<>(2);
-        int frontEndSelector = (int) (Math.random() * frontEnd.size());
-        int backEndSelector = (int) (Math.random() * backEnd.size());
+        int frontEndSelector = new Random().nextInt(frontEnd.size());
+        int backEndSelector = new Random().nextInt(backEnd.size());
+        //int frontEndSelector = (int) (Math.random() * frontEnd.size());
+        //int backEndSelector = (int) (Math.random() * backEnd.size());
         leaders.add(frontEnd.get(frontEndSelector));
         leaders.add(backEnd.get(backEndSelector));
 
         return leaders;
     }
+
+    /**
+     * Getter method to return the members stored of a team.
+     * @return members array of Member instances that store all the members of a team.
+     */
     public Member[] getMembers() {
         return members;
+    }
+
+    /**
+     * Setter method to set the members array.
+     * @param members array of Member instances that store all the members of a team.
+     */
+    public void setMembers(Member[] members) {
+        this.members = members;
     }
 }
